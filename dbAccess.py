b9368af625c5e99d9301ef4cb6867264c3bbb4d8
Ankai Jie <ankaijie@gmail.com>
2016-09-17 19:46:38 -0400

    db api done

--- dbAccess.py
+++ dbAccess.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
 import pymssql
 
-conn = pymssql.connect(tds_version='8.0', server='rapgen2.database.windows.net', user='ankai@rapgen2', password='Hackathon123!', database='rapgen2')  
+conn = pymssql.connect(tds_version='8.0', server='rapgen2.database.windows.net', user='ankai@rapgen2', password='Hackathon123!', database='rapgen2') 
 cursor = conn.cursor()
 
-def addWord(word, syllables, iterFreq=1):
+def addWord(word, syllables, postag, iterFreq=1):
 	iterFreq = str(iterFreq)
 	wordq = '\'' + word + '\''
-	cursor.execute('SELECT * from RapData.Words where word=\'' + word + '\'');
+	postagq = '\'' + postag + '\''
+	cursor.execute('SELECT * from RapData.Words where word=' + wordq);
 	row = cursor.fetchone()
 	if row is None:
-		cursor.execute('Insert into RapData.Words values (' + wordq + ',' + str(syllables) + ', ' + iterFreq + ')')
+		cursor.execute('Insert into RapData.Words values (' + wordq + ',' + str(syllables) + ', ' + iterFreq + ', ' + postagq + ')')
 	else:
 		cursor.execute('UPDATE Rapdata.Words SET frequency = frequency + ' + iterFreq + ' WHERE word = ' + wordq)
 	conn.commit()
@@ -46,27 +47,137 @@ def addToPrevMap(word, prevWord):
 		cursor.execute('UPDATE Rapdata.WordsPrevMap SET frequency = frequency + 1 WHERE edge_id = ' + edge_id)
 	conn.commit()
 
-def addToWords(word, syllables, longestNext, nextSyl, prevWord):
-	addWord(word, syllables)
+def addToWords(word, syllables, longestNext, nextSyl, prevWord, postag, nextPostag):
+	addWord(word, syllables, postag)
 	if longestNext is not None:
-		addWord(longestNext, nextSyl, iterFreq=0)
+		addWord(longestNext, nextSyl, nextPostag, iterFreq=0)
 		addNextLongestMap(word, longestNext)
 
 	if prevWord is not None:
 		addToPrevMap(word, prevWord)
 	conn.commit()
 
+def addToLineStructs(structArr):
+	for struct in structArr:
+		struct = '\'' + struct + '\''
+
+		cursor.execute('SELECT id from RapData.Structures where structure=' + struct);
+		sid = cursor.fetchone()
+		if sid is None:
+			cursor.execute('Insert into RapData.Structures values (' + struct + ', 1 )')
+		else:
+			sid = str(sid[0])
+			cursor.execute('UPDATE Rapdata.Structures SET frequency = frequency + 1 WHERE id = ' + sid)
+
+	conn.commit()
 
 
+def getPrevFromWord(word):
+	wordq = '\'' + word + '\''
+	# cursor.execute('select word_id, w.word, prev_id, prev_word, prev_syllables, \
+	# prev_postag, prevmap.frequency from rapdata.words w \
+	# left join \
+	# 	(select map.word_id, map.prev_id, word.word as prev_word,\
+	# 	word.syllables as prev_syllables, word.postag as prev_postag, map.frequency from rapdata.wordsprevmap map \
+	# 	left join rapdata.words word on map.prev_id = word.id) prevmap\
+	# on w.id = prevmap.word_id where w.word =' + wordq);
 
+	cursor.execute('SELECT id from RapData.words where word=' + wordq)
+	wordid = cursor.fetchone()
+	if word:
+		wordid = str(wordid[0])
+	else:
+		return None
 
+	cursor.execute('select id, word, syllables, postag, map.frequency from rapdata.wordsprevmap map \
+	 left join rapdata.words word on map.prev_id = word.id \
+	 where map.word_id=' + wordid)
 
+	prev_arr=[]
+	row = cursor.fetchone()
+	while row:
+		prev_arr.append({
+			'id': row[0],
+			'word': str(row[1]),
+			'syllables': row[2],
+			'postag': str(row[3]),
+			'frequency': row[4]
+			})
+		row = cursor.fetchone()
 
+	return prev_arr
 
-# cursor.execute('SELECT c.CustomerID, c.CompanyName,COUNT(soh.SalesOrderID) AS OrderCount FROM SalesLT.Customer AS c LEFT OUTER JOIN SalesLT.SalesOrderHeader AS soh ON c.CustomerID = soh.CustomerID GROUP BY c.CustomerID, c.CompanyName ORDER BY OrderCount DESC;')  
-# row = cursor.fetchone()  
-# while row:  
-#     print row
-#     row = cursor.fetchone()
-addToWords('Test', 1, 'Next', 1, 'Previous')
+def getLongsFromWord(word):
+	wordq = '\'' + word + '\''
+
+	cursor.execute('SELECT id from RapData.words where word=' + wordq)
+	wordid = cursor.fetchone()
+	if word:
+		wordid = str(wordid[0])
+	else:
+		return None
+
+	cursor.execute('select id, word, syllables, postag, map.frequency from rapdata.WordsNextLineLongest map \
+	 left join rapdata.words word on map.target_id = word.id \
+	 where map.root_id=' + wordid)
+
+	prev_arr=[]
+	row = cursor.fetchone()
+	while row:
+		prev_arr.append({
+			'id': row[0],
+			'word': str(row[1]),
+			'syllables': row[2],
+			'postag': str(row[3]),
+			'frequency': row[4]
+			})
+		row = cursor.fetchone()
+
+	return prev_arr
+
+def getWordAttributes(word):
+	wordq = '\'' + word + '\''
+	cursor.execute('SELECT id, word, syllables, postag, frequency from RapData.words where word=' + wordq)
+	row = cursor.fetchone()
+	if row:
+		return {
+			'id': row[0],
+			'word': str(row[1]),
+			'syllables': row[2],
+			'postag': str(row[3]),
+			'frequency': row[4]
+		}
+	else:
+		return None
+
+def getWordsWithTag(tag):
+	tagq = '\'' + tag + '\''
+	cursor.execute('SELECT id, word, syllables, postag, frequency from RapData.words where postag=' + tagq)
+	row = cursor.fetchone()
+	wordarr = []
+	while row:
+		wordarr.append({
+			'id': row[0],
+			'word': str(row[1]),
+			'syllables': row[2],
+			'postag': str(row[3]),
+			'frequency': row[4]
+			})
+		row = cursor.fetchone()
+	return wordarr
+
+def getTopTagSequences(limit):
+	cursor.execute('SELECT top ' + str(limit) + ' structure, frequency from RapData.structures order by frequency desc')
+	res = cursor.fetchall()
+	return [{'postag': str(x[0]), 'frequency': x[1]} for x in res]
+
+print(getPrevFromWord('fired'))
+print(getLongsFromWord('fired'))
+print(getWordAttributes('fired'))
+print(getWordsWithTag('NN'))
+print(getTopTagSequences(1))
+
+# addWord('Previous', 3, 'VV')
+# addToWords('Test', 1, 'Next', 1, 'Previous', 'NN', 'NN')
+#addToLineStructs(['VV NN PV', 'VV NN PV', 'NN NN NN'])
 
--- musicToDb.py
+++ musicToDb.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import nltk
 import requests
+import dbAccess
 
 apiString = "&apikey=ca5515a3e14ca1f16d19c54f9a98c5a7"
 url = 'http://api.musixmatch.com/ws/1.1/'
@@ -10,7 +11,7 @@ mGraphString = 'e6a54a7a422f513be9590d65b7c0c618'
 def getAllArtistTracks(name):
 	name = '%20'.join(name.split())
 	query = mGraphApi + 'artist/suggest?api_key=' + mGraphString + '&prefix=' + name + '&limit=1'
-	
+
 	res = requests.get(query)
 	data = res.json()
 	aid = data['data'][0]['id']
@@ -24,7 +25,6 @@ def getAllArtistTracks(name):
 		title_arr.append(str(track['title']))
 
 	return title_arr
-
 
 
 def getArtistId(name):
@@ -109,13 +109,17 @@ def lyricsToData(lyrics):
 		for widx, word in enumerate(linesplit):
 			prevWord = None
 			if widx > 0:
-				prevWord = linesplit[widx-1]
+				prevWord = linesplit[widx-1].lower().replace("'", "")
 
-			word = word.lower()
+			word = word.lower().replace("'", "")
 			longestNext = getLongestWord(nextLine)
 			syllables = count_syl(word)
 			nextSyl = count_syl(longestNext)
 			pos_tag = nltk.pos_tag([word])[0][1][0:2]
+			next_pos_tag = None
+			if longestNext is not None:
+				longestNext = longestNext.lower().replace("'", "")
+				next_pos_tag = nltk.pos_tag([longestNext])[0][1][0:2]
 			wDict = {}
 			wDict['word'] = word
 			wDict['syllables'] = syllables
@@ -123,22 +127,55 @@ def lyricsToData(lyrics):
 			wDict['longNextSyl'] = nextSyl
 			wDict['prevWord'] = prevWord
 			wDict['pos_tag'] = pos_tag
-			print wDict
+			wDict['next_pos_tag'] = next_pos_tag
 			lyricsArr.append(wDict)
 
+	return lyricsArr
+
+def lyricsToLineData(lyrics):
+	lyricsArr = []
+	lyrics = cleanLyrics(lyrics)
+	arr = lyrics.split('\n')
+	for idx, line in enumerate(arr):
+		tags = nltk.pos_tag(line.split())
+		lyricsArr.append(tags)
+
+	tagArr = []
+	for arr in lyricsArr:
+		arr = [x[1][0:2] for x in arr]
+		arr = ' '.join(arr)
+		tagArr.append(arr)
+
+	return tagArr
 
 
-def pushLyricsToDb (arr):
-	return
+def pushWordsToDb (arr):
+	for wDict in arr:
+		print(wDict)
+		dbAccess.addToWords(wDict['word'], wDict['syllables'],wDict['longNext'],
+			wDict['longNextSyl'], wDict['prevWord'], wDict['pos_tag'], 
+			wDict['next_pos_tag'])
+
+def pushLineStructToDb(arr):
+	dbAccess.addToLineStructs(arr)
+
+
+# Push all lyrics data to database
+def pushLyricsToDb(lyrics):
+	wArr = lyricsToData(lyrics)
+	structArr = lyricsToLineData(lyrics)
+	pushWordsToDb(wArr)
+	pushLineStructToDb(structArr)
+
 
 
 
 
 # getTrackLyrics(getTrackId('kanye west', 'amazing'))
 
-# sample_lyrics = "Everybody fired up this evening\nI'm exhausted barely breathing\nHolding on to what I believe in\nNo matter what you'll never take that from me\n\nMy reign is as far as your eyes can see... its amazing\nSo amazing, so amazing\nSo amazing, it's amazing\nSo amazing, so amazing\nSo amazing, it's amazing\nI'm a monster I'm a killer\n\nI know I'm wrong\nI'm a problem\nThat will never ever be solved\nNo matter what you'll never take that from me\n\nMy reign is as far as your eyes can see... it's amazing\nSo amazing, so amazing\nSo amazing, it's amazing\n...\n\n******* This Lyrics is NOT for Commercial use *******"
-# sample_lyrics = "Everybody fired up this evening\nI'm exhausted barely breathing"
-# lyricsToData(sample_lyrics)
+sample_lyrics = "Everybody fired up this evening\nI'm exhausted barely breathing\nHolding on to what I believe in\nNo matter what you'll never take that from me\n\nMy reign is as far as your eyes can see... its amazing\nSo amazing, so amazing\nSo amazing, it's amazing\nSo amazing, so amazing\nSo amazing, it's amazing\nI'm a monster I'm a killer\n\nI know I'm wrong\nI'm a problem\nThat will never ever be solved\nNo matter what you'll never take that from me\n\nMy reign is as far as your eyes can see... it's amazing\nSo amazing, so amazing\nSo amazing, it's amazing\n...\n\n******* This Lyrics is NOT for Commercial use *******"
+sample_lyrics = "Everybody fired up this evening\nI'm exhausted barely breathing"
+pushLyricsToDb(sample_lyrics)
 
 # kanyelist = getAllArtistTracks('kanye west')
 # for song in kanyelist:
