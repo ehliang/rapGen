ca191e5e5d0b0ea5b1cfc27b0cba172fda5bb89e
Ankai Jie <ankaijie@gmail.com>
2016-09-18 03:52:28 -0400

    web ui complete

--- src/routes.js
+++ src/routes.js
@@ -5,9 +5,10 @@ module.exports = function(app, path) {
         res.sendFile(__dirname + '/index.html');
     });
 
-    var filepath = path.join(__dirname, 'thriftshop.m4a');
+    var filepath = path.join(__dirname, 'thriftshop.mp3');
 
     app.get('/music', function(req, res){
+    	console.log('here');
     	res.set({'Content-Type': 'audio/mpeg'});
     	var readStream = fs.createReadStream(filepath);
     	readStream.pipe(res);
